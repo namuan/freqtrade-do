087ce423fadf525033c63f6c8b143d7eadafd3b0
namuan <575441+namuan@users.noreply.github.com>
2020-08-30 21:55:55 +0100

    Update bot start script

--- scripts/start-freqtrade-bot.sh
+++ scripts/start-freqtrade-bot.sh
@@ -1,1 +1,1 @@
-./freqtrade/.env/bin/freqtrade trade -c config.json --strategy CandleStick --logfile freqtrade.log
+./freqtrade/.env/bin/freqtrade trade -c config.json --strategy CandleStickStrategy --logfile freqtrade.log
